aef7e109c93fabe49998a6576fbd3b5a3acfd32a
Shaaarmeyn <sharmainedelapena77@gmail.com>
2025-11-14 16:43:47 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 // ✅ Components
-import Navbar from "./components/navbar.jsx";
+import Navbar from "./components/Navbar.jsx";
 
 // ✅ Pages
 import LandingPage from "./pages/LandingPage.jsx";
 import CarListing from "./pages/CarListing.jsx";
-import OrderPage from "./pages/OrderPage.jsx"; // Make sure this file exists!
+import OrderPage from "./pages/OrderPage.jsx"; 
 
 export default function App() {
   return (
